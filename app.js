920d9d10b107085f6036e926e3c69ddc144ea4e0
Martin <mpatino117@gmail.com>
2017-12-06 17:38:27 -0700

    added error handlers

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 // set routes for modularity
 
 const products_routes = require('./api/routes/products')
 const orders_routes = require('./api/routes/orders')
 
-app.use('/products', products_routes)
+// use morgan to handle request
 
+app.use(morgan('dev'))
+
+app.use('/products', products_routes)
 app.use('/orders', orders_routes)
+
+app.use((req, res, next ) => {
+  let error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
+// handle errors for default route
 
 app.get('*', function (req, res, next) {
   setImmediate(() => {
