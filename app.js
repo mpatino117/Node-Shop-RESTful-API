58508ee5ef136a1e5b30c833ffefc64cad7fd149
Martin <mpatino117@gmail.com>
2017-12-19 00:13:45 -0700

    added moongoDB and moongose

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const moongoose = require('mongoose')
+
+const {
+  MONGO_DB_PW
+} = process.env
+
+console.log(process.env.MONGO_DB_PW, 'test')
+
+moongoose.connect('mongodb://mpatino117:' + MONGO_DB_PW + '@node-rest-shop-shard-00-00-jbfqu.mongodb.net:27017,node-rest-shop-shard-00-01-jbfqu.mongodb.net:27017,node-rest-shop-shard-00-02-jbfqu.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin', {
+  useMongoClient: true
+})
+
 
 // set routes for modularity
 
@@ -12,15 +24,17 @@ const orders_routes = require('./api/routes/orders')
 
 app.use(morgan('dev'))
 // parse url and json to make it readable
-app.use(bodyParser.urlencoded({urlencoded: false}))
+app.use(bodyParser.urlencoded({
+  urlencoded: false
+}))
 app.use(bodyParser.json())
 
 // ensure that we do not have CORSE errors. Security standards for the browser
 
-app.use((req,res, next) =>{
-  res.header('Access-Control-Allow-Origin', '*')  
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization')
-  if(req.method == 'OPTIONS'){
+  if (req.method == 'OPTIONS') {
     res.header('ACCESS-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE')
     return res.status(200).json({})
   }
@@ -30,7 +44,7 @@ app.use((req,res, next) =>{
 app.use('/products', products_routes)
 app.use('/orders', orders_routes)
 
-app.use((req, res, next ) => {
+app.use((req, res, next) => {
   let error = new Error('Not Found')
   error.status = 404
   next(error)
