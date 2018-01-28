326f766e7515529e86fed8f5f31698e216e9404e
Martin <mpatino117@gmail.com>
2018-01-28 12:41:25 -0700

    added json token auth and login

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+
 
 const torch = require('torch')
+
+const {SECRET_JSON_KEY} = process.env
 
 const User = require('../models/user')
 
@@ -12,13 +16,16 @@ router.post('/signup',(req, res, next) => {
   User.find({email: req.body.email})
     .exec()
     .then((response) => {
-      if(response){
-        torch.blue(response)
-       return  res.status(422).json({
+      console.log(response)
+      console.log(response.length)
+      if(response.length >= 1){
+        return  res.status(409).json({
           message:"Email already exists"
         })
+
       } else {
-        bcrypt.hash(req.body.password, 10,(err, hash) => {
+        // bcrypt is the proper way to store passwords in your database regardless of whatever language your backend is built 
+          bcrypt.hash(req.body.password, 10,(err, hash) => {
           if(err){
             return res.status(500).json({
               error:err
@@ -33,7 +40,6 @@ router.post('/signup',(req, res, next) => {
       
             user.save()
               .then(result => {
-                torch.green(result)
                 res.status(201).json({
                   message:'signup success',
                   result:result
@@ -41,7 +47,6 @@ router.post('/signup',(req, res, next) => {
               })
       
               .catch(err => {
-                console.log(err)
                 res.status(500).json({
                   message:'Error signup in',
                   error: err
@@ -53,27 +58,70 @@ router.post('/signup',(req, res, next) => {
   })
 })
 
-router.post('/signin',(req, res, next) => {
+router.post('/login',(req, res, next) => {
+  User.find({email: req.body.email})
+    .exec()
+    .then((user)=>{
+        if(user.length < 1){
+          res.status(401).json({
+            message:'Auth failed '
+          })
+        }
 
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    email: req.body.email,
-    password: req.body.password
-  })
+        bcrypt.compare(req.body.password, user[0].password, (err, result)=> {
+          if(err){
+            return  res.status(401).json({
+              message:'Auth failed '
+            })
+          }
+          if(result){
+            // utilzin jwt sign token and data we wnat in our json token
 
-  user.save()
-  .then(res => {
-    res.status(201).json({
-      message:'signin success',
-      result:res
-    })
+        const token =  jwt.sign({
+              email: user[0].email,
+              id:user[0].password
+            }, 
+            SECRET_JSON_KEY,
+            { expiresIn: '1h' }
+          )
+
+            return res.status(200).json({
+              message:'Auth Successful',
+              token:token
+            })
+          }
+            // if they all fail
+            res.status(401).json({
+              message:'Auth failed '
+          })
+        })
+      // const user = new User({
+      //   _id: new mongoose.Types.ObjectId(),
+      //   email: account.body.email,
+      //   password: account.body.password
+      // })
+
+      // user.save()
+      //   .then(result => {
+      //     res.status(201).json({
+      //       message:'signin success',
+      //       result:result
+      //     })
+      //   })
+      //   .catch(err => {
+      //     res.status(500).json({
+      //       message:'Error signin in',
+      //       error: err
+      //     })
+      //   })
+    
   })
-  .catch(err => {
-    res.status(500).json({
-      message:'Error signin in',
-      error: err
+    .catch((err)=> {
+      console.log(err)
+      res.status(500).json({
+        message:err
+      })
     })
-  })
 })
 
 
